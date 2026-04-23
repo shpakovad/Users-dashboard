408ae898410d2af1ee3b7482fb95ee284bd28c40
Dasha <shpakovad@gmail.com>
2026-04-23 15:06:48 +0300

    Fixed default limit

--- src/shared/api/users.ts
+++ src/shared/api/users.ts
@@ -1,6 +1,6 @@
 export const getUsers = async () => {
     try {
-        const response = await fetch('https://dummyjson.com/users?limit=0');
+        const response = await fetch('https://dummyjson.com/users?limit=100');
         if (!response.ok) {
             return {isError: true, message: 'No Data Found'};
         }
